5a14dd7077ec79988220ab0fe0baa66961f6091c
chspring1 <1069756754@qq.com>
2025-07-30 16:58:38 +0800

    Create main.go

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	result := 0
+
+	nums := []int{2, 2, 1, 1, 3, 3, 4, 4, 5}
+	for _, num := range nums {
+
+		result = num
+		nubmber := 0
+		for _, num := range nums {
+			if result == num {
+				nubmber = nubmber + 1
+			}
+		}
+		if nubmber == 1 {
+			fmt.Println("唯一的数字是sss%:", result)
+			break
+		}
+
+	}
+}
